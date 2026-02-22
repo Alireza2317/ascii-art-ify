ad32b9ac9ff8923d93dfedf90b5a87caaa4a6339
Alireza Alavi <alireza.alavi237@gmail.com>
2026-02-22 23:49:24 +0100

    using the enhanced image to create the art

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+from pathlib import Path
+
 import numpy as np
 
 from src.core.ascii import frame2ascii
-from src.sources.image import ImageType, img2np, load_image, resize_img
+from src.display.terminal import display_ascii_art
+from src.sources.image import ImageType, enhance_image, img2np, load_image, resize_img
 
 
-def pretty_print(mat: np.ndarray) -> None:
-	for row in mat:
-		for char in row:
-			print(char, end=" ")
-		print()
+def save_to_file(ascii_mat: np.ndarray, filename: str | Path) -> None:
+	with open(filename, "w") as file:
+		file.write("\n".join(" ".join(row) for row in ascii_mat) + "\n")
 
 
 if __name__ == "__main__":
 	original_img: ImageType = load_image("images/pic.png", grayscale=True)
 	resized_img: ImageType = resize_img(original_img, 24)
-	mat: np.ndarray = img2np(resized_img) * 1.3
 
+	enhanced_img: ImageType = enhance_image(resized_img)
+	mat: np.ndarray = img2np(enhanced_img)
 	ascii_mat: np.ndarray = frame2ascii(frame=mat)
-
-	pretty_print(ascii_mat)
+	display_ascii_art(ascii_mat)
