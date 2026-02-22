717dd19806e760a798c5eefce870cd28ef37f257
Alireza Alavi <alireza.alavi237@gmail.com>
2026-02-22 05:35:41 +0100

    added option to convert image to black/white

--- src/sources/image.py
+++ src/sources/image.py
@@ -1,16 +1,25 @@
 from pathlib import Path
 
+import numpy as np
 from PIL import Image
 from PIL.Image import Image as ImageType
 
 
-def load_image(path: Path | str) -> ImageType:
+def load_image(path: Path | str, grayscale: bool = False) -> ImageType:
 	image_path: Path = Path(path)
 	if not image_path.exists():
 		raise FileNotFoundError(f"{image_path} does not exist!")
 
 	try:
-		return Image.open(image_path)
+		if grayscale:
+			return Image.open(image_path).convert("L")
+		else:
+			return Image.open(image_path)
 
 	except Exception as e:
 		raise FileNotFoundError(f"Failed to open {image_path}! : {e}")
+
+
+def img2np(img: ImageType) -> np.ndarray:
+	return np.array(img)
+
