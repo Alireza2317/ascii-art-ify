9c3e89590a81136ab044f74a989e96bf77a0f9bf
Alireza Alavi <alireza.alavi237@gmail.com>
2026-02-23 01:27:40 +0100

    added helper functions to main

--- main.py
+++ main.py
@@ -12,11 +12,17 @@ def save_to_file(ascii_mat: np.ndarray, filename: str | Path) -> None:
 		file.write("\n".join(" ".join(row) for row in ascii_mat) + "\n")
 
 
-if __name__ == "__main__":
-	original_img: ImageType = load_image("images/pic.png", grayscale=True)
-	resized_img: ImageType = resize_img(original_img, 24)
-
+def load_ascii_mat_from_image_file(image_path: Path | str, max_dim: int) -> np.ndarray:
+	original_img: ImageType = load_image(image_path, grayscale=True)
+	resized_img: ImageType = resize_img(original_img, max_dim)
 	enhanced_img: ImageType = enhance_image(resized_img)
+
 	mat: np.ndarray = img2np(enhanced_img)
 	ascii_mat: np.ndarray = frame2ascii(frame=mat)
+
+	return ascii_mat
+
+
+if __name__ == "__main__":
+	ascii_mat: np.ndarray = load_ascii_mat_from_image_file("images/pic.png", 24)
 	display_ascii_art(ascii_mat)
