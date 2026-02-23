6eb7c12cabdfab94ca4032f5659f1843bcd9cbc3
Alireza Alavi <alireza.alavi237@gmail.com>
2026-02-23 05:07:32 +0100

    added some routes to flask app

---
+++ src/web/routes.py
@@ -0,0 +1,37 @@
+from flask import Blueprint, render_template, request
+
+from src.core.pipeline import load_ascii_mat_from_image_file
+
+main_blueprint = Blueprint("main", __name__)
+
+
+@main_blueprint.route("/")
+def index():
+	return render_template("index.html")
+
+
+@main_blueprint.route("/upload", methods=["POST"])
+def upload():
+	# Check if an image was uploaded
+	if "image" not in request.files:
+		return "No image selected!", 400
+
+	image_file = request.files["image"]
+
+	# Check if the file is empty
+	if image_file.filename == "":
+		return "No image selected!", 400
+
+	try:
+		ascii_mat = load_ascii_mat_from_image_file(
+			image_file.filename or "", max_dim=200
+		)
+
+		ascii_str: str = "\n".join(" ".join(row) for row in ascii_mat) + "\n"
+		# The <pre> tag preserves whitespace and uses a monospace font.
+		return f"<pre>{ascii_str}</pre>"
+
+	except Exception as e:
+		# It's good practice to handle potential errors during processing
+		print(f"An error occurred: {e}")
+		return "Sorry, something went wrong while processing your image.", 500
